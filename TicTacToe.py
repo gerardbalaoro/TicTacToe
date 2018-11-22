d9ce0477c7c56b3e008b9d686aaa24df9da52354
ghersil <45216730+ghersil@users.noreply.github.com>
2018-11-22 08:43:25 +0800

    Added an incomplete flip mode

--- TicTacToe.py
+++ TicTacToe.py
@@ -102,3 +102,130 @@ class Board:
 			return True
 
 		return False
+
+class FlipMode:
+    """TicTacToe Flip Game Mode"""
+
+    def __init__(self):
+        """Initializes the 4x4 board"""
+
+        self.matrix = []
+        for row in range(4):
+            self.matrix.append([' ' for _ in range(4)])
+
+    def board(self):
+        """
+        Create a copy of the current state of the board matrix
+        Returns:
+             The board matrix in list form
+        """
+        return self.matrix.copy()
+
+    def string(self):
+        """Prints the board in the terminal"""
+        print('\n'.join(['|\t' + '\t|\t'.join([col for col in row]) + '\t|' for row in self.matrix]))
+
+    def row(self, n):
+        """
+        Gets a single row from the board
+        Parameters:
+            n {int} -- a zero based row number
+        Returns:
+            the row in tuple form
+        """
+        return tuple(self.matrix[n])
+
+    def column(self, n):
+        """
+        Gets a single column from the board
+        Parameters:
+            n {int} -- a zero based row number
+        Returns:
+            the column in tuple form
+        """
+        return tuple([row[n] for row in self.matrix])
+
+    def get(self, x, y):
+        """
+        Get cell value based on (x, y) coordinates
+        Parameters:
+            x {int} -- zero-based row number
+            y {int} -- zero-based column number
+        Returns:
+            the cell value
+        """
+        return self.matrix[x][y]
+
+    def set(self, value, x, y):
+        """
+        Sets cell value of based on (x, y) coordinates
+        Parameters:
+            value {str} -- either 'X', 'x', 'O', or 'o'
+            x {int} -- zero-based row number
+            y {int} -- zero-based column number
+        """
+        self.matrix[x][y] = value
+
+    def flip(self, x, y, d):
+        """
+        Flips the opponent's piece in one of the 4 cardinal directions
+        Parameters:
+            x {int} -- zero-based row number
+            y {int} -- zero-based column number
+            d {str} -- either 'up', 'down', 'left', or 'right'
+        """
+        if self.matrix[x][y] == self.matrix[x][y].lower():
+            self.matrix[x][y] = self.matrix[x][y].upper()
+        else:
+            self.matrix[x][y] = self.matrix[x][y].lower()
+
+        if d == 'up':
+            self.matrix[x][y], self.matrix[x-1][y] = ' ', self.matrix[x][y]
+        elif d == 'down':
+            self.matrix[x][y], self.matrix[x+1][y] = ' ', self.matrix[x][y]
+        elif d == 'left':
+            self.matrix[x][y], self.matrix[x][y-1] = ' ', self.matrix[x][y]
+        else:
+            self.matrix[x][y], self.matrix[x][y+1] = ' ', self.matrix[x][y]
+
+    def canflip(self, x, y):
+        """
+        Checks if the cell value can still be flipped
+        Parameters:
+            x {int} -- zero-based row number
+            y {int} -- zero-based column number
+        Returns:
+            A tuple of the directions it can flip to or False
+        """
+        can = []
+
+        if x > 0:
+            if self.matrix[x-1][y] == ' ':
+                can.append('up')
+        if x < 3:
+            if self.matrix[x+1][y] == ' ':
+                can.append('down')
+        if y > 0:
+            if self.matrix[x][y-1] == ' ':
+                can.append('left')
+        if y < 3:
+            if self.matrix[x][y+1] == ' ':
+                can.append('right')
+
+        if len(can) > 0:
+            return tuple(can)
+        else:
+            return False
+
+    def check(self, x, y):
+        """
+        Checks if there is a line of 3 of the same value vertically, horizontally, or diagonally
+        Parameters:
+            x {int} -- zero-based row number
+            y {int} -- zero-based column number
+        Returns:
+            A tuple of the line/s made or False
+        """
+        value = self.matrix[x][y]
+
+        """INCOMPLETE"""
