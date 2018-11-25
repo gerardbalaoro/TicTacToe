90b282dc6e90321693fac7879a5d78ef571c3d69
ghersil <45216730+ghersil@users.noreply.github.com>
2018-11-25 17:26:39 +0800

    resizable=False

--- TicTacToeUnlimited.py
+++ TicTacToeUnlimited.py
@@ -1,6 +1,6 @@
 import gui
 import boards
 
-window = gui.Window(width=800, height=600, caption='Tic Tac Toe Unlimited', resizable=True)
+window = gui.Window(width=800, height=600, caption='Tic Tac Toe Unlimited', resizable=False)
 
 gui.app()
