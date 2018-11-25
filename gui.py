1c483013c9a93afae1ff50dd3830670e9eb5dbc5
ghersil <45216730+ghersil@users.noreply.github.com>
2018-11-25 22:39:16 +0800

    added clicking areas
    for the Main Menu and the Rules screen
    haven't done any board stuff yet
    (sorry I haven't put any comments yet either)

--- gui.py
+++ gui.py
@@ -6,10 +6,12 @@ class Window(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        #self.set_minimum_size(400, 300)
-        #self.set_maximum_size(1600, 1200)
+        # self.set_minimum_size(400, 300)
+        # self.set_maximum_size(1600, 1200)
 
-        self.main_menu()
+        self.background = pyglet.image.load('resources/Main_Menu_final.jpg')
+        self.currentscreen = 'Main Menu'
+        self.main()
 
     """
     #if window is resizable
@@ -26,15 +28,41 @@ class Window(pyglet.window.Window):
 
     def on_draw(self):
         self.clear()
-        
+
         bg = pyglet.sprite.Sprite(self.background, x=0, y=0)
         bg.scale = self.width / 1600
         bg.draw()
 
-    def main_menu(self):
+    def on_mouse_press(self, x, y, button, modifiers):
+        if self.currentscreen == 'Main Menu':
+            if x in range(200, 630) and y in range(315, 350):
+                self.rules('3x3')
+            elif x in range(180, 630) and y in range(230, 265):
+                self.rules('Flip')
+            elif x in range(100, 700) and y in range(150, 190):
+                self.rules('Ulti')
+            elif ((x in range(100, 220) and y in range(45, 70)) or
+                  (x in range(80, 235) and y in range(70, 105))):
+                self.load()
+
+        elif self.currentscreen[0] == 'Rules':
+            if (((x-380)**2)/10) + (((y-90)**2)/5) <= 25**2:
+                print('PLAY')
+            elif ((x in range(560, 760) and y in range(480, 500)) or
+                  (x in range(610, 720) and y in range(500, 545))):
+                self.main()
+        elif self.currentscreen[0] == 'Board':
+            pass
+
+    def main(self):
         self.background = pyglet.image.load('resources/Main_Menu_final.jpg')
+        self.currentscreen = 'Main Menu'
+
+    def load(self):
+        pass
 
     def rules(self, mode):
+        self.currentscreen = ('Rules', mode)
         if mode == '3x3':
             self.background = pyglet.image.load('resources/Rules_of_3x3_final.jpg')
         elif mode == 'Flip':
@@ -42,16 +70,13 @@ class Window(pyglet.window.Window):
         elif mode == 'Ulti':
             self.background = pyglet.image.load('resources/rules_of_ultimate.jpg')
 
-    def board_reg(self):
-        self.background = pyglet.image.load('resources/3X3_game_final.jpg')
-
-    def board_flip(self):
-        self.background = pyglet.image.load('resources/Flip_tac_toe_game.jpg')
-
-    def board_ulti(self):
-        self.background = pyglet.image.load('resources/ultimate_game.jpg')
-        
+    def board(self, mode):
+        if mode == '3x3':
+            self.background = pyglet.image.load('resources/3X3_game_final.jpg')
+        elif mode == 'Flip':
+            self.background = pyglet.image.load('resources/Flip_tac_toe_game.jpg')
+        elif mode == 'Ulti':
+            self.background = pyglet.image.load('resources/ultimate_game.jpg')
 
 def app():
     pyglet.app.run()
-
