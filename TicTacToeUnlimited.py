b2312c7693b0b800b0d9d33f4f498983b8b8f618
Gerard Balaoro <miogerard@yahoo.com>
2018-11-27 16:20:31 +0800

    Update file structure

--- TicTacToeUnlimited.py
+++
@@ -1,6 +0,0 @@
-import gui
-import boards
-
-window = gui.Window(width=800, height=600, caption='Tic Tac Toe Unlimited', resizable=False)
-
-gui.app()
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello")
+import interface, enginee
+
+window = gui.Window(width=800, height=600, caption='Tic Tac Toe Unlimited', resizable=False)
+
+gui.app()
