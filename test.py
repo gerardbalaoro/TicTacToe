874735cd3bb4e7ca81a0fa96fb57cee4f3535c0e
ghersil <45216730+ghersil@users.noreply.github.com>
2018-11-27 17:55:56 +0800

    edited (incomplete)

--- test.py
+++ test.py
@@ -1,56 +1,120 @@
-from engine import *
-
-B = UltimateMode()
-
-locked = False
-ctr = 0
+from boards import *
 
 while True:
-    if ctr % 2 == 0:
-        value = 'x'
-    else:
-        value = 'o'
+    mode = input().lower()
+    if mode in ('flip', 'ultimate'):
+        break
 
-    print(value.upper() + "'s Turn")
-    B.string()
+if mode == 'flip':
 
-    try:
-        inp = input()
-        if inp == 'exit':
-            break
+    B = FlipMode()
 
-        inp = inp.split()
-        if locked:
-            ix, iy = int(inp[0]), int(inp[1])
-        else:
-            x, y, ix, iy = int(inp[0]), int(inp[1]), int(inp[2]), int(inp[3])
+    ctr = 0
+    f = True
 
-        if B.matrix[x][y][ix][iy] == ' ':
+    while True:
+        B.string()
 
-            B.set(value, x, y, ix, iy)
+        ctr = 0
 
-            check = B.check(x, y, ix, iy)
-            if check is not False:
-                B.set(check[0], x, y)
+        try:
+            inp = input()
 
-                check = B.check(x, y)
-                if check is not False:
-                    print('\n')
-                    if check[0] == '-':
-                        print('DRAW')
-                    elif check[0] == 'x':
-                        print('X WINS!')
-                    else:
-                        print('O WINS!')
-                    B.string()
-                    break
+            if inp == 'exit':
+                break
 
-            if B.big[ix][iy] == ' ':
-                locked = True
-                x, y = ix, iy
+            inp = inp.split()
+
+            x, y = int(inp[0]), int(inp[1])
+
+            if ctr  == 0:
+                val = 'x'
             else:
-                locked = False
+                val = 'o'
 
-            ctr += 1
-    except:
-        print('\nINVALID INPUT')
+            if f:
+                if inp[2] in B.canflip(x, y):
+                    B.flip(x, y, inp[2])
+                    f = False
+                    break
+                continue
+
+            else:
+                if inp[2] == '1':
+                    val = val.upper()
+
+            if act != 'c':
+                ex = inp[3]
+
+            if act == 's':
+                B.set(ex, x, y)
+                if B.win(x, y):
+                    B.string()
+                    print('\n' + ex.upper() + ' Wins!!!')
+                    break
+            elif act == 'f':
+                if ex in B.canflip(x, y):
+                    B.flip(x, y, ex)
+            elif act == 'c':
+                print(B.check(x, y))
+            print()
+        except:
+            print('\nINVALID')
+            continue
+
+else:
+
+    B = UltimateMode()
+
+    locked = False
+    ctr = 0
+
+    while True:
+        if ctr % 2 == 0:
+            value = 'x'
+        else:
+            value = 'o'
+
+        print(value.upper() + "'s Turn")
+        B.string()
+
+        try:
+            inp = input()
+            if inp == 'exit':
+                break
+
+            inp = inp.split()
+            if locked:
+                ix, iy = int(inp[0]), int(inp[1])
+            else:
+                x, y, ix, iy = int(inp[0]), int(inp[1]), int(inp[2]), int(inp[3])
+
+            if B.matrix[x][y][ix][iy] == ' ':
+
+                B.set(value, x, y, ix, iy)
+
+                check = B.check(x, y, ix, iy)
+                if check is not False:
+                    B.set(check[0], x, y)
+
+                    check = B.check(x, y)
+                    if check is not False:
+                        print('\n')
+                        if check[0] == '-':
+                            print('DRAW')
+                        elif check[0] == 'x':
+                            print('X WINS!')
+                        else:
+                            print('O WINS!')
+                        B.string()
+                        break
+
+                if B.big[ix][iy] == ' ':
+                    locked = True
+                    x, y = ix, iy
+                else:
+                    locked = False
+
+                ctr += 1
+        except:
+            print('\nINVALID INPUT')
